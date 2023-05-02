fa3ebd890aaa5d172b685030ea9e8a97b1ed563e
shawnWekesa <102484330+ShawnWekesa@users.noreply.github.com>
2023-05-02 13:31:51 +0300

    .

--- random_item_selector/script.js
+++ random_item_selector/script.js
@@ -1,19 +1,18 @@
 const items = document.querySelectorAll(".item");
-	itemsArray = Array.from(items);
+	// itemsArray = Array.from(items);
 
 
 let selected = false;
 let allItemsSelected = false;
 
 function selectItem(){
-	for(i = -1; i < itemsArray.length; i++){
-		let randomIndex = Math.floor(Math.random() * itemsArray.length);
+	for(i = 0; i < items.length; i++){
+		let randomIndex = Math.floor(Math.random() * items.length);
 		console.log(items[randomIndex], randomIndex);
-		items[randomIndex].classList.add('selected');
-		items[randomIndex].style.background = 'red';
+		items[randomIndex].classList.add("selected");
+		
 	};
-}
+};
 
-// let randomIndex = Math.floor(Math.random() * items.length);
-	console.log(typeof(itemsArray));
+
 selectItem();
