137631e4408298d33635050ae4a992376a58b378
shawnWekesa <102484330+ShawnWekesa@users.noreply.github.com>
2024-01-28 23:08:07 +0300

    .

--- random_item_selector/script.js
+++ random_item_selector/script.js
@@ -5,32 +5,27 @@ let itemsArray = document.querySelectorAll(".item");
 let selected = false;
 let allItemsSelected = false;
 
-// function selectItem(){
-// 	while (items.length > 0){
-// 		let randomIndex = Math.floor(Math.random() * items.length);
-// 		items[randomIndex].classList.add("selected");
-// 		console.log(items[randomIndex], randomIndex, items);
-// 		items.splice(randomIndex, 1);
-// 	};
+function selectItem(){
+	while (items.length > 0){
+		let randomIndex = Math.floor(Math.random() * items.length);
+		items[randomIndex].classList.add("selected");
+		// console.log(items[randomIndex], randomIndex, items);
+		items.splice(randomIndex, 1);
+	};
 	// console.log(selectedIndex)
-// };
+};
 
 
-// selectItem();
+selectItem();
 
-// let arr = [1, 2, 3, 4, 5];
+let ourArray = [ 1, 2, 3, 5, 9, 6]
+let condition = ourArray.length
+let dynamicArray = ourArray;
 
-//while (arr.length > 0) {
-//  const element = arr[0];
-  // Perform operation on the element
-//  console.log(`Processing element ${element}`, arr, arr.length);
-  // Remove element from the array
-//  arr.splice(0, 1);
-//}
 
-// const arr = [1, 2, 3, 4, 5];
-// console.log(arr.slice(0, 4))
-// const indexToRemove = 2;
-// const newArray = arr.slice(0, indexToRemove).concat(arr.slice(indexToRemove + 1));
-// console.log(newArray);
-// console.log(arr);
+for(i=0;i<condition;i++){
+	let randomCondition = Math.floor(Math.random()*dynamicArray.length)
+	let removedItem =	dynamicArray.splice(randomCondition, 1);
+	console.log( randomCondition, removedItem, dynamicArray)
+}
+
