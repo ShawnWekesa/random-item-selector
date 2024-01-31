1282cd1bc62ec704f53ed41cf613d06778ae4419
shawnWekesa <102484330+ShawnWekesa@users.noreply.github.com>
2024-01-31 16:33:49 +0300

    .

--- random_item_selector/script.js
+++ random_item_selector/script.js
@@ -5,27 +5,43 @@ let itemsArray = document.querySelectorAll(".item");
 let selected = false;
 let allItemsSelected = false;
 
-function selectItem(){
-	while (items.length > 0){
-		let randomIndex = Math.floor(Math.random() * items.length);
-		items[randomIndex].classList.add("selected");
-		console.log(items[randomIndex], randomIndex, items);
-		items.splice(randomIndex, 1);
-	};
-	// console.log(selectedIndex)
-};
+// function selectItem(){
+// 	while (items.length > 0){
+// 		let randomIndex = Math.floor(Math.random() * items.length);
+// 		items[randomIndex].classList.add("selected");
+// 		console.log(items[randomIndex], randomIndex, items);
+// 		items.splice(randomIndex, 1);
+// 	};
+// };
 
 
-selectItem();
+// selectItem();
 
-let ourArray = [ 1, 2, 3, 5, 9, 6]
-let condition = ourArray.length
-let dynamicArray = ourArray;
+function randomFlash(){
+	if (items.length > 1) {
+		randomIndex = Math.floor(Math.random()*items.length)
+		itemsArray[randomIndex].classList.add("selected");
+		items.splice(randomIndex, 1)
+		console.log(items, itemsArray, randomIndex)
 
-
-for(i=0;i<condition - 1;i++){
-	let randomCondition = Math.floor(Math.random()*dynamicArray.length)
-	let removedItem =	dynamicArray.splice(randomCondition, 1);
-	console.log( randomCondition, removedItem, dynamicArray)
+		setTimeout(randomFlash, 400)
+	}
 }
 
+randomFlash()
+
+let ourArray = [1, 2, 3, 5, 9, 6];
+let dynamicArray = ourArray.slice(); 
+
+function arrey() {
+	if (dynamicArray.length > 1) {
+		let randomCondition = Math.floor(Math.random() * dynamicArray.length);
+    	let removedItem = dynamicArray.splice(randomCondition, 1);
+    	console.log(randomCondition, removedItem, dynamicArray);
+
+    	setTimeout(arrey, 200)
+	}
+}
+
+
+arrey();
