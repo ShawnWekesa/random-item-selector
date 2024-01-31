b4e41a05de8c85d647bdeca9a7f95cd0f830da31
shawnWekesa <102484330+ShawnWekesa@users.noreply.github.com>
2024-01-31 13:03:49 +0300

    .

--- random_item_selector/script.js
+++ random_item_selector/script.js
@@ -9,7 +9,7 @@ function selectItem(){
 	while (items.length > 0){
 		let randomIndex = Math.floor(Math.random() * items.length);
 		items[randomIndex].classList.add("selected");
-		// console.log(items[randomIndex], randomIndex, items);
+		console.log(items[randomIndex], randomIndex, items);
 		items.splice(randomIndex, 1);
 	};
 	// console.log(selectedIndex)
@@ -23,7 +23,7 @@ let condition = ourArray.length
 let dynamicArray = ourArray;
 
 
-for(i=0;i<condition;i++){
+for(i=0;i<condition - 1;i++){
 	let randomCondition = Math.floor(Math.random()*dynamicArray.length)
 	let removedItem =	dynamicArray.splice(randomCondition, 1);
 	console.log( randomCondition, removedItem, dynamicArray)
