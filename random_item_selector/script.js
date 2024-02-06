049cf7c4a83d6fb00165d3966f230742ab0816fd
shawnWekesa <102484330+ShawnWekesa@users.noreply.github.com>
2024-02-06 17:00:37 +0300

    Fisher-Yates shuffle algorithm
    finally shuffled and selected each element at random

--- random_item_selector/script.js
+++ random_item_selector/script.js
@@ -2,8 +2,8 @@ let itemsArray = document.querySelectorAll(".item");
 	items = Array.from(itemsArray);
 
 
-let selected = false;
-let allItemsSelected = false;
+// let selected = false;
+// let allItemsSelected = false;
 
 // function selectItem(){
 // 	while (items.length > 0){
@@ -14,30 +14,40 @@ let allItemsSelected = false;
 // 	};
 // };
 
-
 // selectItem();
 
-function randomFlash(){
-	if (items.length > 1) {
-		randomIndex = Math.floor(Math.random()*items.length)
-		itemsArray[randomIndex].classList.add("selected");
-		items.splice(randomIndex, 1)
-		console.log(items, itemsArray, randomIndex)
+for(i = items.length - 1; i > 0; i--) {
+	const j = Math.floor(Math.random() * (i + 1));
+	[items[i], items[j]] = [items[j], items[i]]
+}
 
-		setTimeout(randomFlash, 400)
+console.log(items)
+
+function randomFlash(){
+	if (items.length > 0) {
+		items[0].classList.add("selected");
+		items.splice(0, 1)
+		console.log(items)
+
+		setTimeout(randomFlash, 200)
 	}
 }
 
 randomFlash()
 
-for (let i = arr.length - 1; i > 0; i--) {
-  const j = Math.floor(Math.random() * (i + 1));
-  [arr[i], arr[j]] = [arr[j], arr[i]];
-}
+// for (let i = arr.length - 1; i > 0; i--) {
+//   const j = Math.floor(Math.random() * (i + 1));
+//   [arr[i], arr[j]] = [arr[j], arr[i]];
+// }
 
-// let ourArray = [1, 2, 3, 5, 9, 6];
+// let ourArray = ["a", "b", "c", "d", "e", "f"];
 // let dynamicArray = ourArray.slice(); 
 
+// for(i = dynamicArray.length - 1; i > 0; i--) {
+// 	const j = Math.floor(Math.random() * (i + 1));
+// 	[dynamicArray[i], dynamicArray[j]] = [dynamicArray[j], dynamicArray[i]]
+// }
+//  console.log(dynamicArray)
 
 // function arrey() {
 // 	if (dynamicArray.length > 1) {
